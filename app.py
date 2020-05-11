f33251a77b76d80322b954069fa5deedfb62e92c
MT Lu <morek999@gmail.com>
2020-05-11 21:23:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def message_text(event):
 	msg_time = datetime.datetime.now() + datetime.timedelta(hours=8)		# Switch timestamp to UTC+8
 	msg_src = event.source.type + "_" + source_id(event.source.type)		# Concatenate source type and source id
 
-	if profile.display_name[0:2] == '修民':
+	if event.source.user_id == 'U608b24e2c1eb1e60eeb36c2c332ed328':
 		line_bot_api.reply_message(event.reply_token, TextSendMessage(text='閉嘴'))
 		return 0
 	elif '臥底' in msg:
